f5cbdb4b391fe391904d609d41d219562ceacd57
corneliusgdahling <corneliusgdahling@gmail.com>
2018-03-19 17:08:22 +0100

    Completed task 3

---
+++ src/exercise_1/task_3.py
@@ -0,0 +1,31 @@
+from utils import get_tweets, result_file, write_results
+from constants import *
+
+
+def centroids(tweets):
+    tuples = tweets.map(lambda x:
+                        (x[COLUMNS.index('country_name')],
+                         1,
+                         float(x[COLUMNS.index('latitude')]),
+                         float(x[COLUMNS.index('longitude')])))\
+        .keyBy(lambda x: x[0])\
+        .aggregateByKey(
+        (0, 0.0, 0.0),
+        (lambda x, y: (x[0] + y[1], x[1] + y[2], x[2] + y[3])),
+        (lambda rdd1, rdd2: (rdd1[0] + rdd2[0], rdd1[1] + rdd2[1], rdd1[2] + rdd2[2]))) \
+        .filter(lambda t: t[1][0] > 10)\
+        .map(lambda t: "%s\t%s\t%s" % (t[0], t[1][1] / t[1][0], t[1][2] / t[1][0]))\
+        .collect()
+
+    return tuples
+
+
+if __name__ == "__main__":
+    task = '3'
+    tweets = get_tweets(task, False)
+
+    result_file = open(result_file(task), "w")
+
+    results = centroids(tweets)
+
+    write_results(result_file, results)
